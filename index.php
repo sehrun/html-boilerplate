cae49f731ae19d8003fa250c2a1f5d4a50e04b6f
sehrun <42497999+sehrun@users.noreply.github.com>
2020-08-21 11:47:15 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+// header("HTTP/1.0 404 Not Found");
+?>
+<!doctype html>
+<html lang=ru>
+<meta charset=utf-8>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>title</title> 
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+
+<link href="/assets/style.css?<?=date('his')?>" rel="stylesheet" />
+
+<div class="hp1">
+	<div class="hp2 cw">
+
+	</div>
+</div>
+
+
+
+
+
+
+<script src="/assets/script.js?<?=date('his')?>"></script>
+
+</html>
+
+	
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
