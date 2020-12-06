426cbdf87286efc373c76c9530ecbae1d4e7933b
sehrun <42497999+sehrun@users.noreply.github.com>
2020-12-07 01:28:57 +0300

    IE, favicon, preconnect

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 // header("HTTP/1.0 404 Not Found");
 ?>
 <!doctype html>
-<html lang=ru>
-<meta charset=utf-8>
+<html lang="ru">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>title</title> 
+<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
 
+<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+<link rel="dns-prefetch" href="https://fonts.gstatic.com/">
+<link rel="preconnect" href="https://use.fontawesome.com/" crossorigin>
+<link rel="dns-prefetch" href="https://use.fontawesome.com/">
+
+<link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,700&amp;subset=cyrillic" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
 
 <link href="/assets/style.css?<?=date('his')?>" rel="stylesheet" />
