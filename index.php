165c3a5e89c56bafb1aee2eae0d4b40bc101b6ae
sehrun <42497999+sehrun@users.noreply.github.com>
2021-06-13 10:42:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -50,6 +50,10 @@ foreach($tempArr as $item){
 
 </div>
 
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
 
 <script src="/assets/script.js?<?=date('his')?>"></script>
 
